ec6b9b4206fa965f72fd4f597034a3b655a0ce6a
Rick <94098966+Evil-Rickz@users.noreply.github.com>
2025-07-16 18:48:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/patao-premiacoes/',
   server: {
     host: "::",
     port: 8080,
